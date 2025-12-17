23dbf5867275b79641ff40e7268a9a59811f2515
lsagarmys1997-crypto <l.sagar.mys1997@gmail.com>
2025-12-17 13:04:56 +0530

    server.js

--- src/server.js
+++ src/server.js
@@ -4,4 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`HotelSuite API running on port ${PORT}`);
+  // Health check (VERY IMPORTANT)
+app.get('/health', (req, res) => {
+  res.json({ alive: true });
 });
